299634ae8b2502fa68c0cb4f96de430e1925562e
Soumalya nandy <soumalyanandy@gmail.com>
2026-07-12 14:11:27 +0530

    Fix - team auth controller toke expire logic

--- config/global.config.js
+++ config/global.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    employee_login_from: '10:00:00',
-    employee_login_to: '12:30:00'
-    
+    employee_login_from: '09:00:00',
+    employee_login_to: '12:30:00',
+
+    // team/employee auth token expires at this time (IST) on the next occurrence.
+    // After this time anyone with the token is rejected (401) and must re-login.
+    employee_token_expire_at: '08:59:59'
+
 }
